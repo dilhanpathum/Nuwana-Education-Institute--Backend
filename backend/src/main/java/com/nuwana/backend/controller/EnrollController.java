348762c86f90163458f4771fa2fe53f8bb453bbd
dilhanpathum <dilhan.jayasiri@gmail.com>
2024-07-07 22:08:45 +0530

    updated

--- backend/src/main/java/com/nuwana/backend/controller/EnrollController.java
+++ backend/src/main/java/com/nuwana/backend/controller/EnrollController.java
@@ -1,11 +1,15 @@
 package com.nuwana.backend.controller;
 
 import com.nuwana.backend.dto.EnrollDto;
+import com.nuwana.backend.dto.UpdateDto;
+import com.nuwana.backend.dto.UserDto;
 import com.nuwana.backend.dto.UsersDto;
 import com.nuwana.backend.service.EnrollService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,5 +31,16 @@ public class EnrollController {
     @GetMapping("/students")
     public List<EnrollDto> students(){
         return enrollService.getAllStudents();
+    }
+
+    @PutMapping("/updateStatus")
+    public ResponseEntity<EnrollDto> profileEdit(@RequestBody @Valid EnrollDto user) {
+        EnrollDto createdUser = enrollService.acceptEnroll(user);
+        return ResponseEntity.ok(createdUser);
+    }
+
+    @GetMapping("/acceptStudents")
+    public List<EnrollDto> acceptStudents(){
+        return enrollService.getAcceptStudents();
     }
 }
